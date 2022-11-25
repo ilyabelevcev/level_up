44743d7dcf3d43aae32895c116860c1d7128aa3e
Ilya <ilyabelevcev00@gmail.com>
2022-11-25 03:58:20 +0300

    edit all files, add carousel, add timer and add only numbers in input

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// carousel
+$('#js-carousel-wrapper').each(function () {
+    var owl = $(this).find('.owl-carousel').owlCarousel({
+        margin: 150,
+        responsive: {
+            400: {
+                items: 1,
+                margin: 0,
+            },
+            800: {
+                items: 2,
+                margin: 100,
+            },
+            1200: {
+                items: 3,
+                margin: 150,
+            }
+        },
+    });
+    $(this).find('.js-prev').on('click', function () {
+      owl.trigger('prev.owl.carousel');
+    });
+    $(this).find('.js-next').on('click', function () {
+      owl.trigger('next.owl.carousel');
+    });
+  });
+
+// only numbers
+$(".tel").keypress(function(event){
+    event = event || window.event;
+    if (event.charCode!=0 && event.charCode!=46 && (event.charCode < 48 || event.charCode > 57) )
+    return false;
+});
+
+// timer
+function blocktimer(idtimer, timetimes, timesnows, member) {
+    var timetimes = timetimes;
+    timetimes = timetimes.split(", ");
+    var timesnows = timesnows;
+    var ts = new Date(timetimes[0], timetimes[1], timetimes[2]);
+    if((new Date()) > ts){
+    ts = (new Date()).getTime() + timesnows;}
+    $(idtimer).countdown({
+    timestamp	: ts,
+    callback	: function(days, hours, minutes, seconds){
+    } }); }
+    
+    $(document).ready(function () {
+    blocktimer('#timer', '2022, 1, 1',  30*60*1000);
+});
